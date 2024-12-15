6204d11bffafe550623ff821bb50764352a12a09
AoDem0 <dsw52708@student.dsw.edu.pl>
2024-12-15 20:42:13 +0100

    trap

--- Assets/trapCollision.cs
+++ Assets/trapCollision.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class trapCollision : MonoBehaviour
 {
 
-    public CombatScript combat;
+    private CombatScript combat;
+    public int trapDMG = 5;
+    public int howQuickDMG = 2;
     // Start is called before the first frame update
     void Start()
     {
         combat = FindObjectOfType<CombatScript>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        {
+            //sa 2 collidery u playera
+            if (collision is BoxCollider)
+            {
+                Debug.Log("Player in trap");
+                combat.current_health -= trapDMG;
+            }
+
+        }
+            
     }
 }
