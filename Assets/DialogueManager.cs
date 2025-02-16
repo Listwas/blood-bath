abaea9a85dfda361c59b03bfdcac80eed0aecaab
AoDem0 <dsw52708@student.dsw.edu.pl>
2025-02-16 21:47:19 +0100

    simple dialog manager

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+ 
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+ 
+    public Image characterIcon;
+    public Text characterName;
+    public Text dialogueArea;
+    public GameObject panelTop;
+    public GameObject panelBottom;
+    private bool doneTyping = false;
+
+ 
+    private Queue<DialogueLine> lines;
+    
+    
+ 
+    public float typingSpeed = 0.2f;
+ 
+
+ 
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+ 
+        lines = new Queue<DialogueLine>();
+        Active(false);
+    }
+
+    //void matchdialogue{
+    //zmiana w dikt
+    //try match with dikt
+    //if matched startdialogue
+    //}
+ 
+    public void StartDialogue(Dialogue dialogue)
+    {
+        
+        Active(true);
+
+ 
+        lines.Clear();
+ 
+        foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
+        {
+            lines.Enqueue(dialogueLine);
+        }
+        
+        DisplayNextDialogueLine();
+        
+        
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+ 
+        DialogueLine currentLine = lines.Dequeue();
+ 
+        characterIcon.sprite = currentLine.character.icon;
+        characterName.text = currentLine.character.name;
+ 
+        StopAllCoroutines();
+ 
+        StartCoroutine(TypeSentence(currentLine));
+    }
+ 
+    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        doneTyping = false;
+        dialogueArea.text = "";
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        {
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        doneTyping = true;
+    }
+    public void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F) && doneTyping)
+        {
+            DisplayNextDialogueLine();
+        }
+    }
+ 
+    void EndDialogue()
+    {
+       Active(false);
+
+    }
+
+    void Active(bool state){
+        characterIcon.gameObject.SetActive(state);
+        characterName.gameObject.SetActive(state);
+        dialogueArea.gameObject.SetActive(state);
+        panelTop.SetActive(state);
+        panelBottom.SetActive(state);
+    }
+}
---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+
+    public string condition;
+    //char sound
+    
+}
+ 
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+ 
+[System.Serializable]
+public class Dialogue
+{
+    //public string dialogueName;
+    //dialog condition?
+    
+    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+}
+[System.Serializable]
+ public class Conditions{
+    public string conditionName;
+    public bool conditionBool;
+ }
+public class DialogueTrigger : MonoBehaviour
+{
+    public List<Dialogue> availableDialogues;
+    public List<Conditions> availableConditions; 
+    
+    
+ 
+    public void TriggerDialogue()
+    {
+        Dialogue selectedDialogue = availableDialogues[UnityEngine.Random.Range(0, availableDialogues.Count)];
+
+        
+        DialogueManager.Instance.StartDialogue(selectedDialogue);
+    }
+ 
+    private void OnTriggerEnter(Collider collision)
+    {
+        // jak wedziemy w trigger to wł ikonke nad npc
+        if (collision is BoxCollider)
+        //and if f 
+            {TriggerDialogue();}
+            
+        
+    }
+
+    
+}
