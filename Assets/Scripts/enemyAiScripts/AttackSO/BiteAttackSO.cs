2c132c20259ad0f270bf9261c57b44219ce1d89c
Oskar Kosecki <okosecki97@gmail.com>
2025-06-03 23:37:12 +0200

    changed the bite attack behaviour
    redid the BiteAttackSO behaviour to be a cone shaped attack with an indicator and some delay.

--- Assets/Scripts/enemyAiScripts/AttackSO/BiteAttackSO.cs
+++ Assets/Scripts/enemyAiScripts/AttackSO/BiteAttackSO.cs
@@ -2,31 +2,47 @@ using UnityEngine;
 using Extensions;
 using UnityEngine.UIElements;
 using UnityEditor.Experimental.GraphView;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using System.Collections;
+using static UnityEngine.UI.Image;
 
 [CreateAssetMenu(menuName = "EnemyAttacks/BiteAttack")]
 public class BiteAttackSO : AttackSO
 {
-    [SerializeField] private Vector3 hitboxSize = new Vector3(2f, 1f, 2f);
-    [SerializeField] private Vector3 hitboxOffset = new Vector3(0, 0.5f, 1f);
+    [Header("Bite indicator values")]
+    [SerializeField] private float biteAngle = 60f;
+    [SerializeField] private float biteRadius = 2f;
+    [SerializeField] private float biteWaitDuration = 1f;
+    [SerializeField] private float biteTriggerRange = 3f;
+    [SerializeField] private Color indicatorColor = Color.red;
 
-    public override float attackRange => hitboxSize.x - hitboxOffset.x;
+    public override float attackRange => biteTriggerRange;
 
     public override void ExecuteAttack(Transform attacker, Transform target, Transform origin, LayerMask targetMask, MonoBehaviour context)
     {
         Debug.Log("ExecuteAttack of BiteSO called!");
-        if(Vector3.Distance(attacker.position, target.position) < 2f)
+        if(Vector3.Distance(attacker.position, target.position) < biteTriggerRange)
         {
             Vector3 directionToPlayer = target.position - attacker.position;
+
+            NavMeshAgent agent = attacker.GetComponent<NavMeshAgent>();
 
             directionToPlayer.y = 0;
 
             if (directionToPlayer != Vector3.zero)
             {
+                context.StartCoroutine(BiteRoutine(attacker, target, agent, targetMask));
+
+                /*
                 //Debug.Log("attacking");
                 Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
                 attacker.rotation = Quaternion.Slerp(attacker.rotation, lookRotation, Time.deltaTime * 5f);
 
                 Vector3 boxCenter = origin.position + attacker.forward * hitboxOffset.z + Vector3.up * hitboxOffset.y;
+
+                Debug.Log("DisplayBiteIndicatorStatic called!");
+                DisplayBiteIndicatorStatic(attacker, target, directionToPlayer);
 
                 // Check if the player is inside the hitbox
                 Collider[] hits = Physics.OverlapBox(boxCenter, hitboxSize, attacker.rotation, targetMask);
@@ -42,9 +58,104 @@ public class BiteAttackSO : AttackSO
                         // You can call TakeDamage() here if needed
                     }
                 }
-
-                DrawHitboxScript.DebugDrawBox(boxCenter, hitboxSize, attacker.rotation, Color.red, 1f);
+                */
             }
         }
     }
+
+    private IEnumerator BiteRoutine(Transform attacker, Transform target, NavMeshAgent agent, LayerMask targetMask)
+    {
+        agent.isStopped = true;
+
+        Vector3 directionToPlayer = (target.position - attacker.position).normalized;
+
+        Debug.Log("DisplayBiteIndicator called!");
+        DisplayBiteIndicatorStatic(attacker, target, directionToPlayer);
+
+        yield return new WaitForSeconds(biteWaitDuration);
+
+        Collider[] hits = Physics.OverlapSphere(attacker.position, biteRadius, targetMask);
+
+        foreach(var hit in hits)
+        {
+            Debug.Log($"Hit: {hit.name}");
+            Vector3 toTarget = (hit.transform.position - attacker.position).normalized;
+
+            float angleToTarget = Vector3.Angle(directionToPlayer, toTarget);
+
+            if (angleToTarget <= biteAngle / 2f)
+            {
+                DealDamage(hit, attacker);
+                Debug.Log($"Hit: {hit.name} damage");
+                break;
+            }
+        }
+
+        agent.isStopped = false;
+        yield return null;
+    }
+
+    private void DisplayBiteIndicatorStatic(Transform attacker, Transform target, Vector3 directionToPlayer)
+    {
+        Debug.Log("Figuring out the bite indicator!");
+
+        GameObject sector = new GameObject("BiteAttackIndicator");
+        sector.transform.position = new Vector3(attacker.position.x, 0.03f, attacker.position.z);
+        sector.transform.rotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0f, directionToPlayer.z));
+
+        MeshFilter mf = sector.AddComponent<MeshFilter>();
+        MeshRenderer mr = sector.AddComponent<MeshRenderer>();
+
+        Mesh sectorMesh = GenerateSectorMesh(biteAngle, biteRadius, 30);
+
+        mf.mesh = sectorMesh;
+
+        Material mat = new Material(Shader.Find("Unlit/Color"));
+        mat.color = indicatorColor;
+        mr.material = mat;
+
+        GameObject.Destroy(sector, biteWaitDuration + 0.01f);
+    }
+
+    private Mesh GenerateSectorMesh(float biteAngle, float biteRadius, int segments)
+    {
+        Debug.Log("Calculating and generating the indicator mesh!");
+
+
+        Mesh mesh = new Mesh();
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        vertices.Add(Vector3.zero); // Tip of the cone (player's position)
+
+        float halfAngle = biteAngle / 2f;
+        float angleRad = biteAngle * Mathf.Deg2Rad;
+        float angleStep = angleRad / segments;
+
+        vertices.Add(Vector3.zero);
+
+        for (int i = 0; i <= segments; i++)
+        {
+            float currentAngle = -halfAngle * Mathf.Deg2Rad + i * angleStep;
+            float x = Mathf.Sin(currentAngle) * biteRadius;
+            float z = Mathf.Cos(currentAngle) * biteRadius;
+            vertices.Add(new Vector3(x, 0f, z));
+        }
+
+        for (int i = 1; i <= segments; i++)
+        {
+            triangles.Add(0);
+            triangles.Add(i);
+            triangles.Add(i + 1);
+        }
+
+        mesh.SetVertices(vertices);
+        mesh.SetTriangles(triangles, 0);
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        return mesh;
+    }
+
 }
